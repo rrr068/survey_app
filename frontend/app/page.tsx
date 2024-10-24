7a21ddd781e5c28182bff19a74627453aed1a7a9
山田燎弥 <ryoya068@yamadaniwabiwatarunoMacBook-Air.local>
2024-10-24 23:28:10 +0900

    アニメーションの追加

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,54 @@
+'use client'
+
 import Header from "@/components/Header"
+import { motion } from "framer-motion"
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Survey App
+      <main>
+        <div className="flex flex-col items-center justify-between p-24">
+          Survey App
+        </div>
+        <div className="pl-6">
+          <motion.h2
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ 
+              type: "spring",
+              duration: 2,
+              delay: 0.5,
+              stiffeness: 600,
+            }}
+            className='text-black lg:text-9xl md:text-8xl font-bold lg:max-w-[40rem] pt-4'>
+              Hello 
+          </motion.h2>
+          <motion.h2
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ 
+              type: "spring",
+              duration: 2,
+              delay: 1.0,
+              stiffeness: 600,
+            }}
+            className='text-black lg:text-9xl md:text-8xl font-bold lg:max-w-[40rem] pl-14 pt-4'>
+              Survey
+          </motion.h2>
+          <motion.h2
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ 
+              type: "spring",
+              duration: 2,
+              delay: 1.5,
+              stiffeness: 600,
+            }}
+            className='text-black lg:text-9xl md:text-8xl font-bold lg:max-w-[40rem] pl-[230px] pt-4'>
+              App
+          </motion.h2>
+        </div>
       </main>
     </>
   )
