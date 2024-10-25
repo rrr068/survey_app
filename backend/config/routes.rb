075f57dc9cf3703743391f71a61f50272b9e4d87
山田燎弥 <ryoya068@yamadaniwabiwatarunoMacBook-Air.local>
2024-10-26 01:17:08 +0900

    簡易的な認証機能作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controller: {
+        registrations: :sesstions, only: [:index]
+      }
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
